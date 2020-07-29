18825563131189b4a9be2b74a175fa30741f1bf8
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2020-07-29 02:35:09 -0700

    actually remove the .embark folder from git

---
+++ embarkArtifacts/contracts/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+"ENSRegistry": require('./ENSRegistry.js').default,
+"Resolver": require('./Resolver.js').default,
+"FIFSRegistrar": require('./FIFSRegistrar.js').default,
+"Migrations": require('./Migrations.js').default,
+};
