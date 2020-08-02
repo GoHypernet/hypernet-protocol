7fdd7ea491d597b8a04ad7da213ebe589d552bcf
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2020-08-02 01:04:17 -0700

    Setup scripts for cli testing

---
+++ scripts/001_setup_channel.js
@@ -0,0 +1,11 @@
+// Contracts contains all embark smart contract instances
+// Web3 contains...web3 (and associated accounts, etc)
+// Logger is Embark's custom logger
+modules.exports = async ({ contracts, web3, logger}) => {
+
+	// This first script sets up the basic payment channel
+	// The two accounts that will be sending transactions: account 1 and account 2
+
+
+
+};
---
+++ scripts/002_advance_channel.js
@@ -0,0 +1,7 @@
+// Contracts contains all embark smart contract instances
+// Web3 contains...web3 (and associated accounts, etc)
+// Logger is Embark's custom logger
+modules.exports = async ({ contracts, web3, logger}) => {
+
+
+};
