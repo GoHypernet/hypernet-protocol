5b73079affc400b32a5927046c50e48acc513bce
Charlie Sibbach <charlie@hyperdyne.io>
2020-08-13 17:51:09 -0700

    Got 3Box imported and sort of typed.

---
+++ protocol/implementations/data/3BoxMessagingRepository.ts
@@ -0,0 +1,17 @@
+import {IMessagingRepository} from "@interfaces/data"
+import { Message } from "@interfaces/objects";
+import {BoxSpace, openSpace} from "3box";
+
+
+export class ThreeBoxMessagingRepository implements IMessagingRepository {
+    protected box: BoxSpace;
+
+    constructor() {
+        this.box = openSpace("SomeSpaceName");
+    }
+
+    public sendMessage(message: Message): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+}
