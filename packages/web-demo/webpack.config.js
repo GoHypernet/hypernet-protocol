baabee26bebe9d42ad1bff258e995682656d5459
ashishgautam <agautam@hamropatro.com>
2020-12-24 10:19:16 +0545

    added hybernet logo alos updated webpack for rendering static path of image

--- packages/web-demo/webpack.config.js
+++ packages/web-demo/webpack.config.js
@@ -33,6 +33,13 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+            name: '[path][name].[ext]',
+        },
+      },
     ],
   },
   resolve: {
