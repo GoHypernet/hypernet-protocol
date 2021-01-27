b3500d9bc362b18aced6636615853aacb3d0467a
Dinesh Bhattarai <dineshdb@dbhattarai.info.np>
2021-01-27 16:25:12 +0545

    chore: Setup coverage allowlist to remove src/tests from coverage report

--- packages/hypernet-core/jest.config.js
+++ packages/hypernet-core/jest.config.js
@@ -12,5 +12,17 @@ module.exports = {
   modulePathIgnorePatterns: [
     "<rootDir>/lib/"
   ],
+  // This does not seem to support blacklisting any folder which means we can't enable parent directory and disable child
+  // We should be using peer directories for coverage and non-coverage tests.
+  collectCoverageFrom: [
+    // Enabling following means we can't disable src/tests from coverage report
+    // "<rootDir>/src/**/*.ts",
+
+    // Add other allowed folders to the list below.
+    "<rootDir>/tests/**/*.ts",
+
+    // Disabled because we don't want it to end up in coverage report,
+    // "<rootDir>/src/tests/**/*.ts",
+  ],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' } )
 };
