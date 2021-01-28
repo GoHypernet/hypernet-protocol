d2ca4cb68b6d2f631cd90401af7829a9236df553
muhammed altinci <mohammad.altenji@gmail.com>
2021-01-29 02:50:37 +0300

    update comment

--- packages/web-demo/src/index.tsx
+++ packages/web-demo/src/index.tsx
@@ -1,7 +1,7 @@
 import HypernetWebIntegration, { IHypernetWebIntegration } from "@hypernetlabs/web-integration";
 
 // Just an implementation to have the front end galileo package for development purposes
-// do npm login and yarn install @hypernetwork/galileo-frontend to make it work
+// do npm login and yarn install @hypernetwork/galileo-frontend to make it work in your local
 /* import "./GalileoFrontend"; */
 
 const client: IHypernetWebIntegration = new HypernetWebIntegration();
