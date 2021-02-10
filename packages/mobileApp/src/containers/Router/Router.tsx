dc7f9ab6116a2d0bec3da9c65e0b712c77bebf21
onurkenis <onurkenis@anadolu.edu.tr>
2021-02-11 00:32:58 +0300

    feat: add linear gradient to top level

--- packages/mobileApp/src/containers/Router/Router.tsx
+++ packages/mobileApp/src/containers/Router/Router.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import LinearGradient from "react-native-linear-gradient";
 import MainHome from "@mobileApp/screens/MainHome";
 import Login from "@mobileApp/screens/Login";
 import Splash from "@mobileApp/screens/Splash";
@@ -8,22 +10,44 @@ import { RootStackParamList, RouterProps } from "@mobileApp/interfaces/container
 
 const Stack = createStackNavigator<RootStackParamList>();
 
+const mainTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+    gradient: ["#262F3F", "#1B202A"],
+  },
+};
+
 const Router: React.FC<RouterProps> = (props: RouterProps) => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={props?.initialRouteName} headerMode="none">
-        <Stack.Screen name="Splash" component={Splash} />
-        <Stack.Screen name="MainHome" component={MainHome} />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            animationEnabled: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <LinearGradient
+      colors={mainTheme.colors.gradient}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+    >
+      <NavigationContainer theme={mainTheme}>
+        <Stack.Navigator initialRouteName={props?.initialRouteName} headerMode="none">
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="MainHome" component={MainHome} />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              animationEnabled: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Router;
