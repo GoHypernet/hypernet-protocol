6e8392de2bdb9e28f5c8c01df7d214bf004c3f6f
csibbach <charlie@hypernetlabs.io>
2021-02-26 12:36:54 -0800

    Implement a persistent ID system, using a "login" from your wallet.

--- packages/hypernet-core/src/implementations/utilities/BrowserNodeProvider.ts
+++ packages/hypernet-core/src/implementations/utilities/BrowserNodeProvider.ts
@@ -1,14 +1,18 @@
-import { BrowserNode } from "@connext/vector-browser-node";
+import { BrowserNode, NonEIP712Message  } from "@connext/vector-browser-node";
 import {
   IBrowserNode,
   IContextProvider,
   ILogUtils,
   IBrowserNodeProvider,
   IConfigProvider,
+  IBlockchainProvider,
+  ILocalStorageUtils,
 } from "@interfaces/utilities";
 import { WrappedBrowserNode } from "./WrappedBrowserNode";
-import { VectorError } from "@interfaces/objects/errors";
-import { ResultAsync } from "@interfaces/objects";
+import { BlockchainUnavailableError, CoreUninitializedError, VectorError } from "@interfaces/objects/errors";
+import { errAsync, HypernetConfig, HypernetContext, okAsync, ResultAsync } from "@interfaces/objects";
+import { ResultUtils } from "@hypernetlabs/utils";
+import { ethers} from "ethers";
 
 export class BrowserNodeProvider implements IBrowserNodeProvider {
   protected browserNodeResult: ResultAsync<IBrowserNode, VectorError | Error> | null;
@@ -16,17 +20,52 @@ export class BrowserNodeProvider implements IBrowserNodeProvider {
   constructor(
     protected configProvider: IConfigProvider,
     protected contextProvider: IContextProvider,
+    protected blockchainProvider: IBlockchainProvider,
     protected logUtils: ILogUtils,
+    protected storageUtils: ILocalStorageUtils, 
   ) {
     this.browserNodeResult = null;
     this.browserNode = null;
   }
-  protected initialize(): ResultAsync<IBrowserNode, VectorError> {
+  protected initialize(): ResultAsync<IBrowserNode, VectorError | CoreUninitializedError> {
     if (this.browserNodeResult == null) {
-      this.browserNodeResult = this.configProvider
-        .getConfig()
-        .andThen((config) => {
+      let config: HypernetConfig;
+      let signer: ethers.providers.JsonRpcSigner;
+      let context: HypernetContext;
+
+      this.browserNodeResult = ResultUtils.combine([this.configProvider.getConfig(),
+        this.blockchainProvider.getSigner(),
+      this.contextProvider.getContext()])
+        .andThen((vals) => {
+          [config, signer, context] = vals;
+
+          // If no account is set in the context, then we have to error out.
+          // Normally we would use getInitializedContext, but in this case, you need
+          // the browser node up and running to get the publicIdentifier, which
+          // is part of what qualifies for getInitializedContext. I do not see
+          // a better way that doesn't involve a huge refactor.
+          if (context.account == null) {
+            return errAsync<string[], CoreUninitializedError>(new CoreUninitializedError("Account is not set in BrowserNodeProvider.initialize"));
+          }
+
+          // Check if the user has a signature in local storage for this account
+          const storedSignature = this.storageUtils.getSessionItem(`account-${context.account}-signature`);
+
+          if (storedSignature != null) {
+            return okAsync<string[], BlockchainUnavailableError>([context.account, storedSignature]);
+          }
+
+          return ResultUtils.combine([ResultAsync.fromPromise(signer.getAddress(),
+          (e) => {return e as BlockchainUnavailableError}),
+          ResultAsync.fromPromise(signer.signMessage(NonEIP712Message),
+          (e) => {return e as BlockchainUnavailableError})]);
+        })
+        .andThen((vals) => {
+          const [account, signature] = vals;
+
+          // Create the browser node
           const vectorBrowserNode = new BrowserNode({
+            routerPublicIdentifier: config.routerPublicIdentifier,
             logger: this.logUtils.getPino(),
             iframeSrc: config.iframeSource,
             chainProviders: config.chainProviders,
@@ -34,9 +73,32 @@ export class BrowserNodeProvider implements IBrowserNodeProvider {
             //messagingUrl: 'localhost:80'
           });
 
+          // Stick it in a wrapper
           this.browserNode = new WrappedBrowserNode(vectorBrowserNode);
 
-          return this.browserNode.init();
+          // Store the signature so you don't have to sign again
+          this.storageUtils.setSessionItem(`account-${account}-signature`, signature);
+
+          // Initialize the browser node
+          return this.browserNode.init(signature, account);
+        })
+        .orElse((e) => {
+          console.log(e);
+          const shouldAttemptRestore = ((e as any).context?.validationError ?? '').includes(
+            'Channel is already setup');
+
+          if (shouldAttemptRestore && this.browserNode != null) {
+            return this.browserNode.getStateChannelByParticipants(config.routerPublicIdentifier, config.chainId)
+            .andThen((channelState) => {
+              if (channelState == null && this.browserNode != null) {
+                return this.browserNode.restoreState(config.routerPublicIdentifier, config.chainId);
+              }
+              return okAsync<void, VectorError>(undefined);
+            })
+          }
+          else {
+            return errAsync(e);
+          }
         })
         .map(() => {
           return this.browserNode as IBrowserNode;
