6429165e7e0f4ae2a2101e25cd098589c5a63834
oguzhankarahan <oguzhankarahan.tr@gmail.com>
2021-05-25 03:10:24 +0300

    fix: front-end build fix

--- packages/voting-app/connector/src/entities/HypernetlabsVotingEntity.ts
+++
@@ -1,9 +0,0 @@
-import VotingConnectorTheGraph from "../connector";
-
-export default class HypernetlabsVotingEntity {
-  protected _connector: VotingConnectorTheGraph
-
-  constructor(connector: VotingConnectorTheGraph) {
-    this._connector = connector
-  }
-}
--- packages/voting-app/connector/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as VotingConnectorTheGraph } from './connector'
-
-export { default as HypernetlabsVoting } from './entities/HypernetlabsVoting'
-export { default as Cast } from './entities/Cast'
-export { default as Vote } from './entities/Vote'
