ba29847c32bd496fe794a249703ae77f08c4bf29
csibbach <charlie@hypernetlabs.io>
2021-06-07 15:36:33 -0700

    Disable ceramic

--- packages/hypernet-core/src/implementations/data/utilities/StorageUtils.ts
+++ packages/hypernet-core/src/implementations/data/utilities/StorageUtils.ts
@@ -1,21 +1,20 @@
-import { inject, injectable } from "inversify";
-import { ResultAsync, okAsync } from "neverthrow";
-
+import { PersistenceError } from "@hypernetlabs/objects";
 import {
   ILogUtils,
   ILogUtilsType,
   ILocalStorageUtils,
   ILocalStorageUtilsType,
 } from "@hypernetlabs/utils";
+import { inject, injectable } from "inversify";
+import { ResultAsync, okAsync } from "neverthrow";
+
+import { IStorageUtils } from "@interfaces/data/utilities";
 import {
   IContextProvider,
   IContextProviderType,
   ICeramicUtils,
   ICeramicUtilsType,
 } from "@interfaces/utilities";
-import { PersistenceError } from "@hypernetlabs/objects";
-
-import { IStorageUtils } from "@interfaces/data/utilities";
 
 @injectable()
 export class StorageUtils implements IStorageUtils {
@@ -28,17 +27,16 @@ export class StorageUtils implements IStorageUtils {
     protected logUtils: ILogUtils,
   ) {}
 
-  public write<T>(
-    keyName: string,
-    data: T,
-  ): ResultAsync<void, PersistenceError> {
+  public write<T>(keyName: string, data: T): ResultAsync<void, never> {
     return this.contextProvider.getContext().andThen((context) => {
-      if (context.metamaskEnabled) {
-        return this.ceramicUtils.writeRecord(keyName, data);
-      } else {
-        this.localStorageUtils.setItem(keyName, JSON.stringify(data));
-        return okAsync(undefined);
+      this.localStorageUtils.setItem(keyName, JSON.stringify(data));
+      if (false) { // context.metamaskEnabled) {
+        this.ceramicUtils.writeRecord(keyName, data).mapErr((err) => {
+          this.logUtils.error(err);
+        });
+        // TODO: More advanced ceramic error handling
       }
+      return okAsync(undefined);
     });
   }
 
