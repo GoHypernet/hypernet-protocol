0a3d565d8decdee7f3838c93a2ea2e208e120d0b
muhammed altinci <mohammad.altenji@gmail.com>
2021-06-08 05:04:38 +0300

    add z index to iframe core

--- packages/web-integration/src/implementations/app/HypernetWebIntegration.tsx
+++ packages/web-integration/src/implementations/app/HypernetWebIntegration.tsx
@@ -131,6 +131,7 @@ export default class HypernetWebIntegration implements IHypernetWebIntegration {
           height: 100%;
           width: 100%;
           background-color: rgba(0,0,0,0.6);
+          z-index: 999999 !important;
         }
         #__hypernet-protocol-iframe-close-icon__ {
           position: absolute;
