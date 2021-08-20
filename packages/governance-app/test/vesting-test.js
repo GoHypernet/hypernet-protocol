d0a9161686eb1e486ce464cedee5287e54e7cf2e
TtheBC01 <todd@hypernetlabs.io>
2021-08-20 18:10:48 +0000

    update to vester test

--- packages/governance-app/test/vesting-test.js
+++ packages/governance-app/test/vesting-test.js
@@ -23,18 +23,22 @@ describe("Vesting", function () {
 
     const award = 100
     const timeNow = Date.now()
-    const startTime = timeNow+10
+    const startTime = timeNow
     const cliffTime = timeNow+30
     const endTime = timeNow+60
-    // deploy hypertoken contract
+    // deploy Vesting contract
     const Vester = await ethers.getContractFactory("Vester");
     const vester = await Vester.deploy(hypertoken.address, addr1.address, award, startTime, cliffTime, endTime);
     await vester.deployTransaction.wait();
+
+    const tx1 = await hypertoken.transfer(vester.address, award)
+    const tx1_reciept = await tx1.wait()
 
     expect(await vester.vestingAmount()).to.equal(award)
     expect(await vester.vestingBegin()).to.equal(startTime)
     expect(await vester.vestingCliff()).to.equal(cliffTime)
     expect(await vester.vestingEnd()).to.equal(endTime)
     expect(await vester.recipient()).to.equal(addr1.address)
+    expect(await hypertoken.balanceOf(vester.address)).to.equal(award)
   });
 });
