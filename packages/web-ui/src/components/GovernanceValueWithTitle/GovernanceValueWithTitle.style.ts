effe494fb2e097cc8056b8f72e7ef7efc582f8a4
onurkenis <onurkenis@anadolu.edu.tr>
2021-09-23 01:44:37 +0300

    style: remove width from value with title component

--- packages/web-ui/src/components/GovernanceValueWithTitle/GovernanceValueWithTitle.style.ts
+++ packages/web-ui/src/components/GovernanceValueWithTitle/GovernanceValueWithTitle.style.ts
@@ -1,15 +1,14 @@
 import { makeStyles } from "@material-ui/core";
 import { colors } from "@web-ui/theme";
 
-export const useStyles = makeStyles((theme) => ({
-  wrapper: (props: any) => ({
-    width: "100%",
+export const useStyles = makeStyles(() => ({
+  wrapper: {
     padding: 16,
     marginBottom: 24,
     backgroundColor: colors.GRAY100,
     border: `2px solid ${colors.GRAY200}`,
     borderRadius: 3,
-  }),
+  },
   title: {
     marginBottom: 28,
     flex: 1,
