13351fb25b3b2f256f6e87536fa97f828f237539
onurkenis <onurkenis@anadolu.edu.tr>
2021-09-24 09:50:32 +0300

    feat: add registry list page

---
+++ packages/user-dashboard/src/pages/Governance/RegistryList/RegistryList.tsx
@@ -0,0 +1,32 @@
+import { Box } from "@material-ui/core";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+import PageWrapper from "@user-dashboard/components/PageWrapper";
+import { useLayoutContext, useStoreContext } from "@user-dashboard/contexts";
+
+const RegistryList: React.FC = () => {
+  const history = useHistory();
+  const { handleError } = useLayoutContext();
+  const { hypernetWebIntegration } = useStoreContext();
+
+  useEffect(() => {
+    hypernetWebIntegration.webUIClient
+      .renderRegistryListWidget({
+        selector: "registry-list-page-wrapper",
+        onRegistryEntryListNavigate: (registryName) => {
+          history.push(`/registries/${registryName}/entries`);
+          console.log("onRegistryEntryListNavigate");
+        },
+      })
+      .mapErr(handleError);
+  }, []);
+
+  return (
+    <PageWrapper label="Registries" isGovernance>
+      <Box id="registry-list-page-wrapper"></Box>
+    </PageWrapper>
+  );
+};
+
+export default RegistryList;
