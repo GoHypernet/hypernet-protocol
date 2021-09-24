db7b5396d21bd2dfa0d20e350b53624f166d584a
onurkenis <onurkenis@anadolu.edu.tr>
2021-09-24 09:50:56 +0300

    feat: add registry entry list page

---
+++ packages/user-dashboard/src/pages/Governance/RegistryEntryList/RegistryEntryList.tsx
@@ -0,0 +1,38 @@
+import { Box } from "@material-ui/core";
+import React, { useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+import PageWrapper from "@user-dashboard/components/PageWrapper";
+import { useLayoutContext, useStoreContext } from "@user-dashboard/contexts";
+
+const RegistryEntryList: React.FC = () => {
+  const history = useHistory();
+  const { handleError } = useLayoutContext();
+  const { hypernetWebIntegration } = useStoreContext();
+  const { registryName } = useParams<{ registryName: string }>();
+
+  useEffect(() => {
+    hypernetWebIntegration.webUIClient
+      .renderRegistryEntryListWidget({
+        selector: "registry-entry-list-page-wrapper",
+        onRegistryEntryDetailsNavigate: (registryName, entryLabel) => {
+          history.push(`/registries/${registryName}/entry/${entryLabel}`);
+          console.log("onRegistryEntryDetailsNavigate");
+        },
+        onRegistryListNavigate: () => {
+          history.push("/registries/");
+          console.log("onRegistryListNavigate");
+        },
+        registryName,
+      })
+      .mapErr(handleError);
+  }, []);
+
+  return (
+    <PageWrapper label="Registry Entries" isGovernance>
+      <Box id="registry-entry-list-page-wrapper"></Box>
+    </PageWrapper>
+  );
+};
+
+export default RegistryEntryList;
