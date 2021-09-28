6c42e8ec92719430af00320a45a5f39937505bb7
onurkenis <onurkenis@anadolu.edu.tr>
2021-09-28 23:25:10 +0300

    feat: add voting power widget

---
+++ packages/web-ui/src/widgets/VotingPowerWidget/VotingPowerWidget.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useAlert } from "react-alert";
+import { Typography } from "@material-ui/core";
+
+import { useStoreContext, useLayoutContext } from "@web-ui/contexts";
+import { useStyles } from "@web-integration/widgets/VotingPowerWidget/VotingPowerWidget.style";
+
+const VotingPowerWidget: React.FC = () => {
+  const alert = useAlert();
+  const { coreProxy } = useStoreContext();
+  const classes = useStyles();
+  const { setLoading } = useLayoutContext();
+  const [votingPower, setVotingPower] = useState<string>("");
+
+  useEffect(() => {
+    coreProxy
+      .getBalances()
+      .map((accounts) => {
+        console.log("accounts VotingPowerWidget: ", accounts);
+        setVotingPower("0.0000");
+      })
+      .mapErr(handleError);
+  }, []);
+
+  const handleError = (err?: Error) => {
+    console.log("handleError err: ", err);
+    setLoading(false);
+    alert.error(err?.message || "Something went wrong!");
+  };
+
+  return (
+    <Typography className={classes.root}>{`${votingPower} votes`}</Typography>
+  );
+};
+
+export default VotingPowerWidget;
