4060f9acef94bea2ec282c68c5bf68ff138b9d0a
onurkenis <onurkenis@anadolu.edu.tr>
2021-09-29 04:06:35 +0300

    feat: add functional pagination to proposal widget

--- packages/web-ui/src/widgets/ProposalsWidget/ProposalsWidget.tsx
+++ packages/web-ui/src/widgets/ProposalsWidget/ProposalsWidget.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Box } from "@material-ui/core";
 import { useAlert } from "react-alert";
 
 import {
+  GovernancePagination,
   GovernanceProposalListItem,
   GovernanceWidgetHeader,
 } from "@web-ui/components";
@@ -10,6 +11,27 @@ import { IProposalsWidgetParams } from "@web-ui/interfaces";
 import { useStoreContext, useLayoutContext } from "@web-ui/contexts";
 import { Proposal } from "@hypernetlabs/objects";
 import DelegateVotesWidget from "@web-ui/widgets/DelegateVotesWidget";
+
+const getPageItemIndexList = (
+  totalItems: number,
+  currentPage: number,
+  itemsPerPage: number,
+) => {
+  const proposalNumberArr: number[] = [];
+  if (totalItems) {
+    for (let i = 1; i <= Math.min(totalItems, itemsPerPage); i++) {
+      // Get indices from current page backwards.
+      const index = totalItems - (currentPage - 1) * itemsPerPage - i + 1;
+      // Proposals starts with index one.
+      if (index > 0) {
+        proposalNumberArr.push(index);
+      }
+    }
+  }
+  return proposalNumberArr;
+};
+
+const PROPOSALS_PER_PAGE = 10;
 
 const ProposalsWidget: React.FC<IProposalsWidgetParams> = ({
   onProposalCreationNavigate,
@@ -22,18 +44,34 @@ const ProposalsWidget: React.FC<IProposalsWidgetParams> = ({
   const [delegateVotesModalOpen, setDelegateVotesModalOpen] =
     useState<boolean>(false);
 
+  const [page, setPage] = useState<number>(1);
+  const [proposalCount, setProposalCount] = useState<number>(0);
+
+  const proposalsNumberArr = useMemo(
+    () => getPageItemIndexList(proposalCount, page, PROPOSALS_PER_PAGE),
+    [proposalCount, page],
+  );
+
   useEffect(() => {
-    coreProxy.getProposalsCount().map((counts) => {
-      console.log("counts: ", counts);
-    });
     coreProxy
-      .getProposals([10, 9, 8])
-      .map((proposals) => {
-        console.log("proposal list: ", proposals);
-        setProposals(proposals);
+      .getProposalsCount()
+      .map((proposalCount) => {
+        setProposalCount(proposalCount);
       })
       .mapErr(handleError);
   }, []);
+
+  useEffect(() => {
+    if (proposalsNumberArr.length) {
+      coreProxy
+        .getProposals(proposalsNumberArr)
+        .map((proposals) => {
+          console.log("proposal list: ", proposals);
+          setProposals(proposals);
+        })
+        .mapErr(handleError);
+    }
+  }, [JSON.stringify(proposalsNumberArr)]);
 
   const handleError = (err?: Error) => {
     console.log("handleError err: ", err);
@@ -73,6 +111,17 @@ const ProposalsWidget: React.FC<IProposalsWidgetParams> = ({
           status={proposal.state}
         />
       ))}
+      {proposalCount && (
+        <GovernancePagination
+          customPageOptions={{
+            itemsPerPage: PROPOSALS_PER_PAGE,
+            totalItems: proposalCount,
+          }}
+          onChange={(_, page) => {
+            setPage(page);
+          }}
+        />
+      )}
       {delegateVotesModalOpen && (
         <DelegateVotesWidget
           onCloseCallback={() => setDelegateVotesModalOpen(false)}
