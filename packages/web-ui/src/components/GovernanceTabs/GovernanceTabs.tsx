9070971bbee68cea27cce4b1d4322ecba43390eb
Baris Hantas <barhantas@gmail.com>
2021-10-05 22:06:57 +0300

    update tabs style and favico

---
+++ packages/web-ui/src/components/GovernanceTabs/GovernanceTabs.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Tab, Tabs, TabProps, TabsTypeMap } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+interface IGovernanceTabPanel {
+  children: React.ReactNode;
+  value: number;
+  index: number;
+}
+
+export const GovernanceTabs = withStyles((theme) => ({
+  root: {
+    marginBottom: 24,
+  },
+}))((props: TabsTypeMap["props"]) => <Tabs {...props} />);
+
+export const GovernanceTab = withStyles((theme) => ({
+  root: {
+    textTransform: "none",
+    minHeight: 64,
+  },
+}))((props: TabProps) => <Tab disableRipple {...props} />);
+
+export const GovernanceTabPanel: React.FC<IGovernanceTabPanel> = ({
+  children,
+  value,
+  index,
+  ...other
+}: IGovernanceTabPanel) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box>{children}</Box>}
+    </div>
+  );
+};
--- packages/web-ui/src/components/index.ts
+++ packages/web-ui/src/components/index.ts
@@ -44,3 +44,4 @@ export * from "./GovernanceCard";
 export * from "./GovernanceBalanceList";
 export * from "./GovernanceSideFilter";
 export * from "./GovernancePagination";
+export * from "./GovernanceTabs";
