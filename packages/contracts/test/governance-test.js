54b24b456f5bd50a577f2da332206fad9d921d5c
TtheBC01 <todd@hypnetlabs.io>
2021-10-06 19:32:23 +0000

    update

--- packages/contracts/test/governance-test.js
+++ packages/contracts/test/governance-test.js
@@ -2,117 +2,110 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat")
 
 describe("Governance", function () {
-  it("Test governance functions.", async function () {
-    // get signers
-    const [owner, addr1] = await ethers.getSigners()
+    let hypertoken;
+    let timelock;
+    let hypernetgovernor;
+    let owner;
+    let addr1; 
 
-    // deploy hypertoken contract
-    const Hypertoken = await ethers.getContractFactory("Hypertoken");
-    const hypertoken = await Hypertoken.deploy();
-    hypertoken_reciept = await hypertoken.deployTransaction.wait();
-    const totalSupply = await hypertoken.totalSupply()
-    console.log("Hypertoken Address:", hypertoken.address)
-    console.log("Hypertoken Supply:", totalSupply.toString())
-    console.log("Hypertoken Gas Fee:", hypertoken_reciept.gasUsed.toString())
+    beforeEach(async () => {
+        [owner, addr1] = await ethers.getSigners()
 
-    // all tokens are currently owned by owner signer
-    // delegate all votes to self
-    // if delegate() is not called, the account has no voting power
-    const txvotes = await hypertoken.delegate(owner.address)
-    const txvotes_receipt = txvotes.wait()
+        const Hypertoken = await ethers.getContractFactory("Hypertoken");
+        hypertoken = await Hypertoken.deploy();
+        await hypertoken.deployTransaction.wait();
 
-    // deploy timelock contract
-    const Timelock = await ethers.getContractFactory("TimelockController");
-    const timelock = await Timelock.deploy(1, [], []);
-    const timelock_reciept = await timelock.deployTransaction.wait();
-    console.log("Timelock Address:", timelock.address)
-    console.log("Timelock Gas Fee:", timelock_reciept.gasUsed.toString())
+        // deploy timelock contract
+        const Timelock = await ethers.getContractFactory("TimelockController");
+        timelock = await Timelock.deploy(1, [], []);
+        await timelock.deployTransaction.wait();
 
-    // deploy hypernet governor contract
-    const HypernetGovernor = await ethers.getContractFactory("HypernetGovernor");
-    const hypernetgovernor = await HypernetGovernor.deploy(hypertoken.address, timelock.address);
-    const hypernetgovernor_reciept = await hypernetgovernor.deployTransaction.wait();
-    console.log("Governor address:", hypernetgovernor.address)
-    console.log("Governor Gas Fee:", hypernetgovernor_reciept.gasUsed.toString())
-    console.log("Governor name is:", await hypernetgovernor.name())
+        // deploy hypernet governor contract
+        const HypernetGovernor = await ethers.getContractFactory("HypernetGovernor");
+        hypernetgovernor = await HypernetGovernor.deploy(hypertoken.address, timelock.address);
+        await hypernetgovernor.deployTransaction.wait();
 
-    // give the governor contract the Proposer role in the timelock contract
-    const tx1 = await timelock.grantRole(timelock.PROPOSER_ROLE(), hypernetgovernor.address)
-    const tx1_reciept = await tx1.wait()
+        // give the governor contract the Proposer role in the timelock contract
+        let tx = await timelock.grantRole(timelock.PROPOSER_ROLE(), hypernetgovernor.address);
+        tx.wait();
 
-    // give the governor contract the Executor role in the timelock contract
-    const tx2 = await timelock.grantRole(timelock.EXECUTOR_ROLE(), hypernetgovernor.address)
-    const tx2_reciept = await tx2.wait()
+        // give the governor contract the Executor role in the timelock contract
+        tx = await timelock.grantRole(timelock.EXECUTOR_ROLE(), hypernetgovernor.address);
+        tx.wait();
 
-    // deployer address should now renounce admin role for security
-    const tx3 = await timelock.renounceRole(timelock.TIMELOCK_ADMIN_ROLE(), owner.address)
-    const tx3_reciept = await tx3.wait()
+        // deployer address should now renounce admin role for security
+        tx = await timelock.renounceRole(timelock.TIMELOCK_ADMIN_ROLE(), owner.address);
+        await tx.wait();
+	});
 
-    // give some tokens to the Governor contract
-    const tx4 = await hypertoken.transfer(hypernetgovernor.address, 1000)
-    const tx4_reciept = await tx4.wait()
-    expect(await hypertoken.balanceOf(hypernetgovernor.address)).to.equal(1000);
+    it("Test vote delegation.", async function () {
+        let tx = await hypertoken.delegate(owner.address);
+        tx.wait();
 
-    // hardhat extension to etheres, get token metadata
-    const token = await ethers.getContractAt('Hypertoken', hypertoken.address);
-    const governor = await ethers.getContractAt('HypernetGovernor', hypernetgovernor.address);
-    const governorWithSigner = governor.connect(owner)
+        // give some tokens to the timelock contract
+        tx = await hypertoken.transfer(timelock.address, ethers.utils.parseEther("1000"))
+        tx.wait()
 
-    // Example of proposal creation, this one transfers tokens from governor contract
-    // to a target address
-    // create proposal call data
-    const proposalDescription = "Proposal #1: Give grant to address" // Human readable description
-    const descriptionHash = ethers.utils.id(proposalDescription); // Hash description to help compute the proposal ID
-    const transferCalldata = token.interface.encodeFunctionData('transfer', [addr1.address, 7]); // encode the function to be called 
-    const proposalID = await governorWithSigner.hashProposal(
-        [token.address],
-        [0],
-        [transferCalldata],
-        descriptionHash
-    ); // pre-compute the proposal ID for easy lookup later
+        // if delegate() is not called, the account has no voting power
+        expect(await hypertoken.balanceOf(timelock.address)).to.equal(ethers.utils.parseEther("1000"));
+        expect(await hypertoken.getVotes(timelock.address)).to.equal(ethers.utils.parseEther("0"));
+        expect(await hypertoken.getVotes(owner.address)).to.equal(ethers.utils.parseEther("99999000"));
 
-    // propose a vote
-    const tx5 = await governorWithSigner["propose(address[],uint256[],bytes[],string)"](
-        [token.address],
-        [0],
-        [transferCalldata],
-        proposalDescription
-    );
-    const tx5_reciept = await tx5.wait()
+    });
 
-    // give some tokens to addr1 to force a new block in testing environment
-    const tx6 = await hypertoken.transfer(addr1.address, 1000)
-    const tx6_reciept = await tx6.wait()
-    expect(await hypertoken.balanceOf(addr1.address)).to.equal(1000);
+    it("Test governance proposal.", async function () {
 
-    // check state of proposal, should be 1 for Active
-    expect(await governorWithSigner.state(proposalID)).to.equal(1);
+        let tx = await hypertoken.delegate(owner.address);
+        tx.wait();
 
-    const tx7 = await governorWithSigner.castVote(proposalID, 1)
-    const tx7_reciept = tx7.wait()
+        // Example of proposal creation, this one transfers tokens from governor contract
+        // to a target address
+        // create proposal call data
+        let proposalDescription = "Proposal #1: Give grant to address" // Human readable description
+        let descriptionHash = ethers.utils.id(proposalDescription); // Hash description to help compute the proposal ID
+        let transferCalldata = hypertoken.interface.encodeFunctionData('transfer', [addr1.address, 7]); // encode the function to be called 
+        let proposalID = await hypernetgovernor.hashProposal(
+            [hypertoken.address],
+            [0],
+            [transferCalldata],
+            descriptionHash
+        ); // pre-compute the proposal ID for easy lookup later
 
-    // check who has voted
-    expect(await governorWithSigner.hasVoted(proposalID, owner.address)).to.equal(true);
-    expect(await governorWithSigner.hasVoted(proposalID, addr1.address)).to.equal(false);
+        // propose a vote
+        let governorWithSigner = hypernetgovernor.connect(owner)
+        tx = await hypernetgovernor["propose(address[],uint256[],bytes[],string)"](
+            [hypertoken.address],
+            [0],
+            [transferCalldata],
+            proposalDescription
+        );
+        tx.wait()
 
-    // get the blocknumber for a Proposal (i.e "timestamp" it was submitted on)
-    const snapshot = await governorWithSigner.proposalSnapshot(proposalID);
-    console.log("Proposal Start Block:", snapshot.toString());
+        // give some tokens to addr1 to force a new block in testing environment
+        tx = await hypertoken.transfer(addr1.address, 1000)
+        tx.wait()
+        expect(await hypertoken.balanceOf(addr1.address)).to.equal(1000);
 
-    // get the blocknumber at which the Proposal voting period is done
-    const deadline = await governorWithSigner.proposalDeadline(proposalID);
-    console.log("Proposal Block Deadline:", deadline.toString());
+        // check state of proposal, should be 1 for Active
+        expect(await hypernetgovernor.state(proposalID)).to.equal(1);
 
-    // get the required number of votes for successful quorum at a particular block number
-    const quorum = await governorWithSigner.quorum(snapshot);
-    console.log("Proposal Quorum:", quorum.toString());
+        tx = await hypernetgovernor.castVote(proposalID, 1)
+        tx.wait()
 
-    // get Proposal details (defined in GovernorCompatibilityBravo.sol)
-    const proposal = await governorWithSigner.proposals(proposalID);
-    console.log("Proposal Originator:", proposal[1]);
-    console.log("Proposal ETA:", proposal[2].toString());
-    console.log("Proposal Votes For:", proposal[5].toString());
-    console.log("Proposal Votes Against:", proposal[6].toString());
-    console.log("Proposal Executed:", proposal[8]);
-  });
+        // check who has voted
+        expect(await hypernetgovernor.hasVoted(proposalID, owner.address)).to.equal(true);
+        expect(await hypernetgovernor.hasVoted(proposalID, addr1.address)).to.equal(false);
+
+        // get the blocknumber for a Proposal (i.e "timestamp" it was submitted on)
+        const snapshot = await hypernetgovernor.proposalSnapshot(proposalID);
+
+        // get the blocknumber at which the Proposal voting period is done
+        const deadline = await hypernetgovernor.proposalDeadline(proposalID);
+
+        // get the required number of votes for successful quorum at a particular block number
+        const quorum = await hypernetgovernor.quorum(snapshot);
+
+        // get Proposal details (defined in GovernorCompatibilityBravo.sol)
+        const proposal = await hypernetgovernor.proposals(proposalID);
+    });
 });
