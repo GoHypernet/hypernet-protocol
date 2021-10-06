60610831c47aaae512937e4fa643caa676ffa2ab
onurkenis <onurkenis@anadolu.edu.tr>
2021-10-06 21:10:13 +0300

    style: modify hamburger menu position & transition

--- packages/user-dashboard/src/components/MobileOthersMenu/MobileOthersMenu.style.ts
+++ packages/user-dashboard/src/components/MobileOthersMenu/MobileOthersMenu.style.ts
@@ -2,11 +2,15 @@ import { colors } from "@hypernetlabs/web-ui";
 import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles({
+  menu: {
+    marginTop: 48,
+    transition: "all 0.3s ease",
+  },
   menuItem: {
     textTransform: "uppercase",
   },
   iconButton: {
-    color: colors.BLACK
+    color: colors.BLACK,
   },
   activeMenuItem: {
     color: colors.PURPLE700,
