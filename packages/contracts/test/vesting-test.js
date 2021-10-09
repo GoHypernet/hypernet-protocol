0def76e519cf7edd50ef48f43ef772fa6803b1e0
TtheBC01 <todd@hypnetlabs.io>
2021-10-09 00:24:28 +0000

    update to vesting tests

--- packages/contracts/test/vesting-test.js
+++ packages/contracts/test/vesting-test.js
@@ -7,14 +7,20 @@ describe("Vesting", function () {
     let owner;
     let addr1; 
 
+    let startTime;
+    let cliffTime;
+    let endTime;
+
     const award = ethers.utils.parseEther("100");
-    const timeNow = Date.now();
-    const startTime = timeNow;
-    const cliffTime = timeNow+30;
-    const endTime = timeNow+60;
 
     beforeEach(async () => {
         [owner, addr1] = await ethers.getSigners();
+
+        const latestBlock = await hre.ethers.provider.getBlock("latest");
+        const timeNow = latestBlock.timestamp;
+        startTime = timeNow+10; 
+        cliffTime = timeNow+30;
+        endTime = timeNow+60; 
 
         // deploy hypertoken contract
         const Hypertoken = await ethers.getContractFactory("Hypertoken");
@@ -25,14 +31,22 @@ describe("Vesting", function () {
         const Vester = await ethers.getContractFactory("Vester");
         vester = await Vester.deploy(hypertoken.address, addr1.address, award, startTime, cliffTime, endTime);
         await vester.deployTransaction.wait();
-	});
 
-    it("Test vesting contract.", async function () {
         let tx = await hypertoken.transfer(vester.address, award);
         tx.wait();
+	});
 
-        tx = await vester.connect(addr1).delegate(addr1.address);
+    it("Check vesting parameters.", async function () {
+
+        let tx = await vester.connect(addr1).delegate(addr1.address);
         tx.wait();
+
+        let asdf = await vester.vestingBegin();
+        let qwer = await vester.vestingCliff();
+        console.log(startTime)
+        console.log(asdf.toString());
+        console.log(qwer.toString());
+        console.log()
 
         expect(await vester.vestingAmount()).to.equal(award);
         expect(await vester.vestingBegin()).to.equal(startTime);
@@ -41,5 +55,21 @@ describe("Vesting", function () {
         expect(await vester.recipient()).to.equal(addr1.address);
         expect(await hypertoken.balanceOf(vester.address)).to.equal(award);
         expect(await hypertoken.getVotes(addr1.address)).to.equal(award);
+    });
+
+    it("Check for full withdrawal.", async function () {
+        let tx = await vester.connect(addr1).delegate(addr1.address);
+        tx.wait();
+
+        let a = await vester.timeNow();
+        hre.timeAndMine.setTimeIncrease("1d");
+        hre.timeAndMine.mine("100");
+        let b = await vester.timeNow();
+        console.log(a.toString())
+        console.log(b.toString())
+
+        tx = await vester.connect(addr1).claim(); 
+        tx.wait();
+        expect(await hypertoken.balanceOf(addr1.address)).to.equal(award);
     });
 });
