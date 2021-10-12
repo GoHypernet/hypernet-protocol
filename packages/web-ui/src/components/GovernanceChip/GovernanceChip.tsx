c3098b5c90681c366451656ce07aa40d27a84af5
onurkenis <onurkenis@anadolu.edu.tr>
2021-10-12 14:39:41 +0300

    feat: add chip component

---
+++ packages/web-ui/src/components/GovernanceChip/GovernanceChip.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Chip, ChipProps } from "@material-ui/core";
+
+import { useStyles } from "@web-ui/components/GovernanceChip/GovernanceChip.style";
+import { colors } from "@web-ui/theme";
+
+type ChipPropsExtacted = Omit<ChipProps, "color">;
+
+export type ChipColorTypes = "blue" | "gray" | "green" | "orange";
+
+export interface GovernanceChipProps extends ChipPropsExtacted {
+  color?: ChipColorTypes;
+}
+
+export const GovernanceChip: React.FC<GovernanceChipProps> = (
+  props: GovernanceChipProps,
+) => {
+  const { label, color = "gray", ...rest } = props;
+  const colorCodes = getChipColorCodes(color);
+  const classes = useStyles(colorCodes);
+
+  return <Chip className={classes.chip} label={label} {...rest} />;
+};
+
+const getChipColorCodes = (type: ChipColorTypes) => {
+  switch (type) {
+    case "blue":
+      return {
+        backgroundColor: colors.BLUE100,
+        color: colors.BLUE700,
+      };
+    case "green":
+      return {
+        backgroundColor: colors.GREEN100,
+        color: colors.GREEN700,
+      };
+    case "orange":
+      return {
+        backgroundColor: colors.ORANGE100,
+        color: colors.ORANGE700,
+      };
+    case "gray":
+      return {
+        backgroundColor: colors.GRAY200,
+        color: colors.GRAY500,
+      };
+  }
+};
