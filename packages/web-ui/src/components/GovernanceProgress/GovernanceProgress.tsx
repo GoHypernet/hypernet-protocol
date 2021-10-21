fcbde683f30e86debe77a8c09c6aa7166aa0b9a7
onurkenis <onurkenis@anadolu.edu.tr>
2021-10-21 16:18:31 +0300

    style: remove progress useStyles & move rules to theme

--- packages/web-ui/src/components/GovernanceProgress/GovernanceProgress.tsx
+++ packages/web-ui/src/components/GovernanceProgress/GovernanceProgress.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { LinearProgress, ThemeProvider } from "@material-ui/core";
+import { LinearProgress } from "@material-ui/core";
 
-import {
-  progressTheme,
-  useStyles,
-} from "@web-ui/components/GovernanceProgress/GovernanceProgress.style";
 import { colors } from "@web-integration/theme";
 
 interface GovernanceProgressProps {
@@ -17,16 +13,12 @@ export const GovernanceProgress: React.FC<GovernanceProgressProps> = (
   props: GovernanceProgressProps,
 ) => {
   const { value, color = colors.GRAY500, height = 6 } = props;
-  const classes = useStyles();
 
   return (
-    <ThemeProvider theme={progressTheme}>
-      <LinearProgress
-        style={{ backgroundColor: color, height }}
-        className={classes.wrapper}
-        variant="determinate"
-        value={value}
-      />
-    </ThemeProvider>
+    <LinearProgress
+      style={{ backgroundColor: color, height }}
+      variant="determinate"
+      value={value}
+    />
   );
 };
