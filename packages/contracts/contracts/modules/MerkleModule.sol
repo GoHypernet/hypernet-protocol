be4b756310d442ae7351dc9d988f90899f01d912
TtheBC01 <todd@hypnetlabs.io>
2021-10-30 00:45:49 +0000

    update to MerkleDrop.sol

--- packages/contracts/contracts/modules/MerkleModule.sol
+++ packages/contracts/contracts/modules/MerkleModule.sol
@@ -5,20 +5,28 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleModule {
-    // root proof of merkle tree
-    bytes32 immutable public root;
+
     string public name; 
 
     constructor(bytes32 merkleroot, string memory _name)
     {
-        root = merkleroot;
+
         name = _name; 
     }
 
+    /// @notice redeem offload gas cost of minting to recieveres
+    /// @dev scalable distribution of NFIs
+    /// @param to address of the recipient of the token
+    /// @param label a unique label to attach to the token
+    /// @param registrationData data to store in the tokenURI
+    /// @param tokenId unique uint256 identifier for the newly created token
+    /// @param proof merkle traversal proof (see merkletreejs)
+    /// @param registry address of the target NFR
     function redeem(address to, string calldata label, string calldata registrationData, uint256 tokenId, bytes32[] calldata proof, address registry)
     external
     {
-        require(_verify(_leaf(to, label, registrationData, tokenId), proof), "MerkleModule: Invalid merkle proof");
+        bytes32 root = INfr(registry).merkleRoot();
+        require(_verify(_leaf(to, label, registrationData, tokenId), proof, root), "MerkleModule: Invalid merkle proof");
         INfr(registry).register(to, label, registrationData, tokenId);
     }
 
@@ -28,7 +36,7 @@ contract MerkleModule {
         return keccak256(abi.encodePacked(to, label, registrationData, tokenId));
     }
 
-    function _verify(bytes32 leaf, bytes32[] memory proof)
+    function _verify(bytes32 leaf, bytes32[] memory proof, bytes32 root)
     internal view returns (bool)
     {
         return MerkleProof.verify(proof, root, leaf);
@@ -38,4 +46,5 @@ contract MerkleModule {
 // minimal interface for the NonFungibleRegistry register function
 interface INfr {
     function register(address to, string calldata label, string calldata registrationData, uint256 tokenId) external;
+    function merkleRoot() external view returns (bytes32);
 }
