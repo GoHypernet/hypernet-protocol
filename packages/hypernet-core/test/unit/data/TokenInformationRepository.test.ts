2cfa7cc6d6f1f4c853105a4bba9d67690a8d6ed2
csibbach <charlie@hypernetlabs.io>
2021-11-17 15:11:54 -0800

    Added tests for TokenInformationRepository and ResultUtils.combine

--- packages/hypernet-core/test/unit/data/TokenInformationRepository.test.ts
+++ packages/hypernet-core/test/unit/data/TokenInformationRepository.test.ts
@@ -8,14 +8,19 @@ import {
   BigNumberString,
   UnixTimestamp,
   SortedTransfers,
+  NonFungibleRegistryContractError,
 } from "@hypernetlabs/objects";
 import { ILogUtils } from "@hypernetlabs/utils";
 import { TokenInformationRepository } from "@implementations/data";
 import { ITokenInformationRepository } from "@interfaces/data";
 import {
   tokenRegistryAddress,
+  tokenRegistryId1,
+  tokenRegistryId2,
   tokenRegistryEntry1,
   tokenRegistryEntry2,
+  hyperTokenAddress,
+  chainId,
 } from "@mock/mocks";
 import { okAsync, errAsync } from "neverthrow";
 import td from "testdouble";
@@ -80,5 +85,118 @@ describe("TokenInformationRepository tests", () => {
     // Assert
     expect(result).toBeDefined();
     expect(result.isErr()).toBeFalsy();
+  });
+
+  test("initialize() returns NonFungibleRegistryContractError when totalSupply() fails", async () => {
+    // Arrange
+    const mocks = new TokenInformationRepositoryMocks();
+
+    const srcError = new NonFungibleRegistryContractError();
+    td.when(mocks.contract.totalSupply()).thenReturn(errAsync(srcError));
+
+    const repo = mocks.factoryTokenInformationRepository();
+
+    // Act
+    const result = await repo.initialize();
+
+    // Assert
+    expect(result).toBeDefined();
+    expect(result.isErr()).toBeTruthy();
+    const error = result._unsafeUnwrapErr();
+    expect(error).toBe(srcError);
+  });
+
+  test("initialize() returns NonFungibleRegistryContractError when tokenByIndex() fails", async () => {
+    // Arrange
+    const mocks = new TokenInformationRepositoryMocks();
+
+    const srcError = new NonFungibleRegistryContractError();
+    td.when(mocks.contract.tokenByIndex(1)).thenReturn(errAsync(srcError));
+
+    const repo = mocks.factoryTokenInformationRepository();
+
+    // Act
+    const result = await repo.initialize();
+
+    // Assert
+    expect(result).toBeDefined();
+    expect(result.isErr()).toBeTruthy();
+    const error = result._unsafeUnwrapErr();
+    expect(error).toBe(srcError);
+  });
+
+  test("initialize() returns NonFungibleRegistryContractError when getRegistryEntryByTokenId() fails", async () => {
+    // Arrange
+    const mocks = new TokenInformationRepositoryMocks();
+
+    const srcError = new NonFungibleRegistryContractError();
+    td.when(
+      mocks.contract.getRegistryEntryByTokenId(tokenRegistryId1),
+    ).thenReturn(errAsync(srcError));
+
+    const repo = mocks.factoryTokenInformationRepository();
+
+    // Act
+    const result = await repo.initialize();
+
+    // Assert
+    expect(result).toBeDefined();
+    expect(result.isErr()).toBeTruthy();
+    const error = result._unsafeUnwrapErr();
+    expect(error).toBe(srcError);
+  });
+
+  test("getTokenInformation() returns without errors", async () => {
+    // Arrange
+    const mocks = new TokenInformationRepositoryMocks();
+    const repo = mocks.factoryTokenInformationRepository();
+
+    // Act
+    const result = await repo.initialize().andThen(() => {
+      return repo.getTokenInformation();
+    });
+
+    // Assert
+    expect(result).toBeDefined();
+    expect(result.isErr()).toBeFalsy();
+    const chainInformation = result._unsafeUnwrap();
+
+    expect(chainInformation.length).toBe(2);
+  });
+
+  test("getTokenInformationForChain() returns without errors", async () => {
+    // Arrange
+    const mocks = new TokenInformationRepositoryMocks();
+    const repo = mocks.factoryTokenInformationRepository();
+
+    // Act
+    const result = await repo.initialize().andThen(() => {
+      return repo.getTokenInformationForChain(chainId);
+    });
+
+    // Assert
+    expect(result).toBeDefined();
+    expect(result.isErr()).toBeFalsy();
+    const chainInformation = result._unsafeUnwrap();
+
+    expect(chainInformation.length).toBe(2);
+  });
+
+  test("getTokenInformationByAddress() returns without errors", async () => {
+    // Arrange
+    const mocks = new TokenInformationRepositoryMocks();
+    const repo = mocks.factoryTokenInformationRepository();
+
+    // Act
+    const result = await repo.initialize().andThen(() => {
+      return repo.getTokenInformationByAddress(hyperTokenAddress);
+    });
+
+    // Assert
+    expect(result).toBeDefined();
+    expect(result.isErr()).toBeFalsy();
+    const chainInformation = result._unsafeUnwrap();
+
+    expect(chainInformation?.address).toBe(hyperTokenAddress);
   });
 });
