7ad2c723dbf03f9dc605f738477f5bd41493f60c
onurkenis <onurkenis@anadolu.edu.tr>
2021-12-07 20:54:01 +0300

    feat: add mobile view to public id & state channel

--- packages/web-ui/src/widgets/PublicIdentifierWidget/PublicIdentifierWidget.style.ts
+++ packages/web-ui/src/widgets/PublicIdentifierWidget/PublicIdentifierWidget.style.ts
@@ -1,13 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles({
-  dialogSelect: {
-    marginBottom: 0,
-  },
   titleContainer: {
     margin: "auto 0",
-  },
-  form: {
-    flex: 1,
   },
 });
