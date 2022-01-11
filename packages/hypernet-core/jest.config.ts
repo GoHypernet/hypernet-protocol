be07f687147fc69e319253645059b3da513024a0
onurkenis <onurkenis@anadolu.edu.tr>
2022-01-11 18:56:54 +0300

    chore: add ipfs urls to jest config

--- packages/hypernet-core/jest.config.ts
+++ packages/hypernet-core/jest.config.ts
@@ -39,6 +39,8 @@ const config: Config.InitialOptions = {
     __AUTH_URL__: "http://localhost:5040",
     __VALIDATOR_IFRAME_URL__: "http://localhost:5005",
     __CERAMIC_NODE_URL__: "https://ceramic-clay.3boxlabs.com",
+    __IPFS_API_URL__: "http://localhost:5021",
+    __IPFS_GATEWAY_URL__: "http://localhost:8088",
     __GOVERNANCE_CHAIN_ID__: "1337",
     __DEBUG__: true,
   },
