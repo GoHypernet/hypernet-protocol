cd19132f4ce36e6bc4787a7bcd04159c908706cd
onurkenis <onurkenis@anadolu.edu.tr>
2022-02-02 04:33:50 +0300

    feat: update web demo for initialize reject case

--- packages/web-demo/src/index.tsx
+++ packages/web-demo/src/index.tsx
@@ -35,40 +35,49 @@ style.innerHTML = `body {
   background-color: #F9F9F9;
 }`;
 
+
 // Get the first script tag
 const ref = document.querySelector("script");
 ref?.parentNode?.insertBefore(style, ref);
+
 
 Spinner();
 // Spinner.show();
 
 const gatewayUrl = GatewayUrl("http://localhost:3000/users/v0");
 
-client
-  .getReady()
-  .map((coreProxy) => {
-    Spinner.hide();
-    client.webUIClient
-      .startOnboardingFlow({
-        gatewayUrl: gatewayUrl,
-        finalSuccessContent:
-          'You are good to go now and purchase credits from <a href="http://localhost:9000/settings/credits">here</a>',
-        showInModal: true,
-        excludeCardWrapper: true,
-        successButtonProps: {
-          label: `Sucess baby`,
-          action: () => {
-            console.log(`done`);
+const getReady = () => {
+  client
+    .getReady()
+    .map((coreProxy) => {
+      Spinner.hide();
+      client.webUIClient
+        .startOnboardingFlow({
+          gatewayUrl: gatewayUrl,
+          finalSuccessContent:
+            'You are good to go now and purchase credits from <a href="http://localhost:9000/settings/credits">here</a>',
+          showInModal: true,
+          excludeCardWrapper: true,
+          successButtonProps: {
+            label: `Sucess baby`,
+            action: () => {
+              console.log(`done`);
+            },
           },
-        },
-      })
-      .map(() => {
-        Spinner.hide();
-      })
-      .mapErr((err) => {
-        console.log("startOnboardingFlow errerrerr", err);
-      });
-  })
-  .mapErr((err) => {
-    console.log("getReady errerrerr", err);
-  });
+        })
+        .map(() => {
+          Spinner.hide();
+        })
+        .mapErr((err) => {
+          console.log("startOnboardingFlow errerrerr", err);
+        });
+    })
+    .mapErr((err) => {
+      console.log("getReady errerrerr", err);
+    });
+};
+
+const button = document.createElement("button");
+button.innerHTML = "init";
+button.onclick=getReady
+document.body.appendChild(button);
