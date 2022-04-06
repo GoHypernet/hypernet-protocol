beae3c79eb267fb60f12886469df53431b19e5aa
muhammed altinci <muhammed.altinci@gmail.com>
2022-04-06 04:23:42 +0300

    add batchRegisterAsync

--- packages/governance-sdk/src/IBatchModuleContract.ts
+++ packages/governance-sdk/src/IBatchModuleContract.ts
@@ -3,10 +3,16 @@ import {
   RegistryEntry,
   EthereumContractAddress,
   GasUnits,
+  TransactionNotImplementedError,
+  TransactionServerError,
+  TransactionTimeoutError,
+  TransactionUnknownError,
+  TransactionUnsupportedOperationError,
 } from "@hypernetlabs/objects";
 import { ResultAsync } from "neverthrow";
 
 import { ContractOverrides } from "@governance-sdk/ContractOverrides";
+import { ethers } from "ethers";
 
 export interface IBatchModuleContract {
   batchRegister(
@@ -14,7 +20,19 @@ export interface IBatchModuleContract {
     registryEntries: RegistryEntry[],
     overrides?: ContractOverrides | null,
   ): ResultAsync<void, BatchModuleContractError>;
-
+  batchRegisterAsync(
+    registryAddress: EthereumContractAddress,
+    registryEntries: RegistryEntry[],
+    overrides?: ContractOverrides | null,
+  ): ResultAsync<
+    ethers.providers.TransactionResponse,
+    | TransactionNotImplementedError
+    | TransactionServerError
+    | TransactionTimeoutError
+    | TransactionUnknownError
+    | TransactionUnsupportedOperationError
+    | BatchModuleContractError
+  >;
   estimateGasbatchRegister(
     registryAddress: EthereumContractAddress,
     registryEntries: RegistryEntry[],
